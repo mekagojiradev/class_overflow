f24bf40955a1e03b642f8b476459bcb22a67eadf
Braeden512 <braeden4328@gmail.com>
2025-05-03 18:20:03 -0500

    Ayush fix

--- api/register.php
+++ api/register.php
@@ -7,13 +7,12 @@ require 'db.php'; // Include the database connection
 
 // changed this line bc ayush was having problem loading
 header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Content-Type: application/json");
 
 // If it's a preflight request, return early
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
     exit();
 }
 

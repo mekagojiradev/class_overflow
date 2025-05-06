cd644ef63faa14021131f6bb489fe710454b0e1a
Eliab <147437693+EliabW@users.noreply.github.com>
2025-05-05 22:35:54 -0500

    current work so far

--- api/create_response.php
+++ api/create_response.php
@@ -19,21 +19,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 $data = json_decode(file_get_contents("php://input"), true);
 
 // Check if all required fields are provided
-if (isset($data['title']) && isset($data['content']) && isset($data['user_id']) && isset($data['school_id'])) {
-    $title = $data['title'];
+if (isset($data['content']) && isset($data['user_id'])) {
     $content = $data['content'];
     $user_id = $data['user_id'];
-    $school_id = $data['school_id'];
 
-    // Check if the class exists
-    $stmt = $conn->prepare("SELECT id FROM classes WHERE name = ?");
-    $stmt->bind_param("s", $class_name);
-    $stmt->execute();
-    $result = $stmt->get_result();
 
     // Prepare the SQL query to insert the new post
-    $stmt = $conn->prepare("INSERT INTO questions (title, content, user_id, school_id) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssiii", $title, $content, $user_id, $school_id); // Bind the parameters
+    $stmt = $conn->prepare("INSERT INTO responses (content, user_id, question_id) VALUES (?, ?, ?)");
+    $stmt->bind_param("sii", $content, $user_id, $question_id); // Bind the parameters
 
     try {
         $stmt->execute(); // Execute the query
@@ -48,6 +41,7 @@ if (isset($data['title']) && isset($data['content']) && isset($data['user_id']) 
         ]);
     }
 } else {
+
     echo json_encode(["success" => false, "error" => "Missing fields."]); // Missing data error
 }
 ?>
